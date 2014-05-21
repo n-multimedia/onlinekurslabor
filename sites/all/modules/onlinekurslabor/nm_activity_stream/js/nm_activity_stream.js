852c812a72ee84c651b7e9701268db9fc02a957b
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2014-05-21 17:34:17 +0200

    activity_stream: styling, js

--- sites/all/modules/onlinekurslabor/nm_activity_stream/js/nm_activity_stream.js
+++ sites/all/modules/onlinekurslabor/nm_activity_stream/js/nm_activity_stream.js
@@ -7,11 +7,13 @@
     
     
 
-    Drupal.behaviors.nm_stream = {
+    Drupal.behaviors.nm_stream_activity = {
         /*toggle solution for qaa questions*/
         attach: function(context, settings) {
-
-            
+             /* cursor auf pointer setzen und aus-einnklappen realisieren*/
+    jQuery("div.single_activity.seen").css("cursor", "pointer").click(function(){
+        jQuery(this).find(".seen_hidden").slideToggle();
+    });
 
         }
     };
