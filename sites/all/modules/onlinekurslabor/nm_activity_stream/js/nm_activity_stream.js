2d073449a252976a9adb8dfbc2c490acc00a838a
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2014-05-22 11:56:40 +0200

    activity-stream: bugfix JS, Styling

--- sites/all/modules/onlinekurslabor/nm_activity_stream/js/nm_activity_stream.js
+++ sites/all/modules/onlinekurslabor/nm_activity_stream/js/nm_activity_stream.js
@@ -11,7 +11,7 @@
         /*toggle solution for qaa questions*/
         attach: function(context, settings) {
              /* cursor auf pointer setzen und aus-einnklappen realisieren*/
-    jQuery("div.single_activity.seen").css("cursor", "pointer").click(function(){
+    jQuery("div.single_activity.seen").css("cursor", "pointer").once('nm_stream_activity').click(function(){
         jQuery(this).find(".seen_hidden").slideToggle();
     });
 
