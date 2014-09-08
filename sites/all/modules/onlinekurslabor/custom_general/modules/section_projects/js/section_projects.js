25afc612a637ccda87a361c39a65efa14d0155c4
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2014-09-08 12:21:45 +0200

    commit

--- sites/all/modules/onlinekurslabor/custom_general/modules/section_projects/js/section_projects.js
+++ sites/all/modules/onlinekurslabor/custom_general/modules/section_projects/js/section_projects.js
@@ -1,6 +1,6 @@
 (function($) {
 
-  Drupal.behaviors.section_domains = {
+  Drupal.behaviors.section_projects = {
 
     attach: function(context, settings) {
       $(".nav_tabbox .tab").click(function()
@@ -21,7 +21,66 @@
         
         return false;
       });
-            
+      
+      //seal logic
+
+      $('.projects-seal-widget .action a').once('section_projects').click(function(){
+        var self = $(this);
+        var container = self.closest('.action');
+        var href = self.attr('href');
+
+        //ajax action
+        if(href.indexOf('projects/') > 0) {
+
+            var url = href;
+            var tml_html = container.find("> a").first().html();
+            var post_spinner_black;
+            var opts = {
+                      lines: 8, // The number of lines to draw
+                      length: 2, // The length of each line
+                      width: 3, // The line thickness
+                      radius: 4, // The radius of the inner circle
+                      corners: 1, // Corner roundness (0..1)
+                      rotate: 0, // The rotation offset
+                      direction: 1, // 1: clockwise, -1: counterclockwise
+                      color: '#333', // #rgb or #rrggbb or array of colors
+                      speed: 1.2, // Rounds per second
+                      trail: 60, // Afterglow percentage
+                      shadow: false, // Whether to render a shadow
+                      hwaccel: false, // Whether to use hardware acceleration
+                      className: 'spinner', // The CSS class to assign to the spinner
+                      zIndex: 2e9, // The z-index (defaults to 2000000000)
+                      top: 'auto', // Top position relative to parent in px
+                      left: 'auto' // Left position relative to parent in px
+            };
+
+            if (!post_spinner_black) {
+                //first call -> init spinner
+                post_spinner_black = new Spinner(opts).spin();
+            } else {
+                post_spinner_black.spin();
+            }
+
+            container.append(post_spinner_black.el);
+            //container.find(" > a").first().html(tml_html + html_spin);
+            //console.log(url);
+
+            $.ajax({
+              type: "GET",
+              url: url,
+              success: function(response)
+              {
+
+                container.html(response.data);
+                Drupal.attachBehaviors(response);
+                post_spinner_black.stop();
+
+              }
+            });
+
+          return false;
+        }
+      });
     }
   };
 
