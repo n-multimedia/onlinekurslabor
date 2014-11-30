8c9540197822fb253b693b6ff2bd7c2100a2d567
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2014-11-30 15:01:31 +0100

    tagebuch/blog permissions logics

--- sites/all/modules/onlinekurslabor/custom_general/modules/section_projects/templates/section_projects_tools.tpl.php
+++ sites/all/modules/onlinekurslabor/custom_general/modules/section_projects/templates/section_projects_tools.tpl.php
@@ -10,7 +10,7 @@
     </ul>
   <?php endif; ?>
 
-  
+
   <!--
   <?php if (!empty($context_items)): ?>
     <?php echo $context_items['title'] ?>
@@ -28,9 +28,9 @@
   $tool_links = array();
   $count = 0;
   foreach($context_items['items'] as $item){
-        
+
     $attributes = array('attributes' => array('id' => $item['name'], 'class' => array('btn')));
-    dpm($attributes);
+
     if (isset($item['path'])) {
       custom_general_append_active_class(($item['path']), $attributes);
     }
@@ -38,12 +38,12 @@
     $tool_link['group']['root'] = true;
     $tool_link['link'] = $item['link'];
     $tool_link['attributes'] = $attributes['attributes'];
-    
-    
+
+
     $tool_links[] = $tool_link;
-    
+
   }
-  
+
  echo _custom_general_theme_tools_block($tool_links, "projects_tools", "btn-group btn-group-vertical", 1);
 ?>
-  
+
