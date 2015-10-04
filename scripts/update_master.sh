3917292d1659fedd6d86e8a8192562caab3c73bf
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2015-10-04 20:49:54 +0200

    privacy widget for admins and moderators

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -1,13 +1,16 @@
 ##!/bin/sh
 
 #enable modules
-drush en section_projects_features notification_features email_registration htmlmail --yes
+drush en section_projects_features notification_features email_registration htmlmail nm_activity_stream --yes
 
 #revert features
 drush fra --yes
 
 drush updatedb
 
+
+#rebuild node access
+drush php-eval 'node_access_rebuild();'
 
 #clear cache
 drush cc all
