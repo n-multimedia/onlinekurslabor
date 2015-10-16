cb96fbdc73ba26c3d1a7c76ba57d557921c742fd
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2015-10-16 15:33:49 +0200

    update script maintenance mode added

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -3,6 +3,8 @@
 
 drush dl h5p-7.x-1.0-beta4 --yes
 
+drush vset maintenance_mode 1
+
 #enable modules
 drush en section_projects_features notification_features email_registration htmlmail nm_activity_stream --yes
 
@@ -18,3 +20,6 @@ drush php-eval 'node_access_rebuild();'
 
 #clear cache
 drush cc all
+
+
+drush vset maintenance_mode 0
