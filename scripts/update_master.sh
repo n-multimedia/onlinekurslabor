d1dad53e3c3719c9f5ff1d40d4884c6e993197df
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2015-10-16 14:47:49 +0200

    update script h5p beta4

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -1,7 +1,7 @@
 ##!/bin/sh
 
 
-drush dl h5p-7.x-1.0-beta5 --yes
+drush dl h5p-7.x-1.0-beta4 --yes
 
 #enable modules
 drush en section_projects_features notification_features email_registration htmlmail nm_activity_stream --yes
