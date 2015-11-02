773c0c1b370d350b1737001404e98b15f31c444a
Sergej <sergej.naumenko@phil.uni-augsburg.de>
2015-11-02 12:10:19 +0100

    fixed tabscookies

--- web/sites/all/themes/bootstrap_onlinekurslabor/js/tabscookie.js
+++ web/sites/all/themes/bootstrap_onlinekurslabor/js/tabscookie.js
@@ -7,28 +7,33 @@
 
     Drupal.behaviors.okl_bootstrap_tabscookie = {
         /*toggle solution for qaa questions*/
-        attach: function(context, settings) {
+        attach: function(context, settings) { 
             var tabsid = "#tabs-0-center";
             var cookieid = "tabs_0_center_selected";
             if ($(tabsid).length > 0) {
                 $(tabsid).tabs({
-                    show: function(event, ui) {
+                    activate:function(event,ui){
                         //expire tab cookie in 30 minutes
                         var edate = new Date();
                         var minutes = 30;
                         edate.setTime(edate.getTime() + (minutes * 60 * 1000));
-                        var tabname = $(ui.tab).html()
-                        $.cookie(cookieid, tabname, {
-                            expires: edate
-                        });
+                        var tabname = jQuery(ui.newTab.context).html();
+                        if(typeof tabname === null)
+                       	 throw new Error('Selected tab could not be found in /sites/all/themes/bootstrap_onlinekurslabor/js/tabcookie')
+                       	 else
+                       	 {
+                       	     $.cookie(cookieid, tabname, {
+                        		    expires: edate
+                     			   });
+													}
+                    
                     },
                 });
                 var tabs_tmp = $(tabsid + ' li');
                 $.each(tabs_tmp, function(i, val) {
                     if($(val).find('a').html() == $.cookie(cookieid)) {
-                        $(tabsid).tabs("select", i);
+                        $(tabsid).tabs( "option", "active", i);
                     }
-                    //console.log();
                 });
                 
             }
