1629c88236359f89e39cb6f751d7b00ee69ea8d0
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2015-12-04 15:35:13 +0100

    updatedb added to update master

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -7,11 +7,12 @@ drush en userprotect defaultavatar --yes
 drush fr nm_section_courses_features nm_general_features --yes
 
 
-
 #clear cache
 drush cc all
 
 drush image-flush
+
+drush updatedb --yes
 
 drush language-import de ../language/alpha3.po --replace
 
