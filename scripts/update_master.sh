3b3b0ccadef77b2f30da218c035eaec03de03723
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2016-01-08 14:54:42 +0100

    (0/5) WYSIWYG - Plugins

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -6,7 +6,6 @@ drush en userprotect defaultavatar dev_live_warner --yes
 
 drush fr nm_section_content_features nm_section_courses_features nm_general_features --yes
 
-
 #clear cache
 drush cc all
 
@@ -15,6 +14,9 @@ drush image-flush --all
 drush updatedb --yes
 
 drush language-import-translations de ../language/alpha3.po --replace --groups=default
+
+#rebuild node access
+drush php-eval 'node_access_rebuild();'
 
 
 #drush vset maintenance_mode 0
