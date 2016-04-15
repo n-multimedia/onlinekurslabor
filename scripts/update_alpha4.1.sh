73957ada9a904c7c72d1bf7c67c91eb98e10a2f9
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2016-04-15 14:13:23 +0200

    update and localization header

--- scripts/update_alpha4.1.sh
+++ scripts/update_alpha4.1.sh
@@ -1,12 +1,12 @@
 ##!/bin/sh
 
-#drush vset maintenance_mode 1
+drush vset maintenance_mode 1
 drush en h5p_connector_api --yes
 drush en h5p_text_annotations --yes
 drush fr nm_general_features --yes
 drush fr nm_section_courses_features --yes
 drush fr nm_section_content_features --yes
-drush fr nm_section_projects_features --yes
+drush fr section_projects_features --yes
 drush fr nm_h5p_features --yes
 
 
@@ -22,4 +22,4 @@ drush updatedb --yes
 drush language-import-translations de ../language/alpha4.po --replace --groups=default
 
 
-#drush vset maintenance_mode 0
+drush vset maintenance_mode 0
--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -1,11 +1,10 @@
 ##!/bin/sh
 
-#drush vset maintenance_mode 1
-
+drush vset maintenance_mode 1
 drush fr nm_general_features --yes
 drush fr nm_section_courses_features --yes
 drush fr nm_section_content_features --yes
-drush fr nm_section_projects_features --yes
+drush fr section_projects_features --yes
 drush fr nm_h5p_features --yes
 
 
@@ -19,4 +18,4 @@ drush updatedb --yes
 drush language-import-translations de ../language/alpha5.po --replace --groups=default
 
 
-#drush vset maintenance_mode 0
+drush vset maintenance_mode 0
