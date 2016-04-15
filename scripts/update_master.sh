5d8fa7c727c15d912806646f2b1da043fdb88d3a
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2016-04-15 13:38:31 +0200

    new updatescript

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -6,6 +6,8 @@ drush en h5p_connector_api --yes
 drush en h5p_text_annotations --yes
 drush fr nm_general_features --yes
 drush fr nm_section_courses_features --yes
+drush fr nm_section_projects_features --yes
+drush fr nm_h5p_features --yes
 
 #clear cache
 drush cc all
@@ -14,7 +16,7 @@ drush cc all
 drush updatedb --yes
 
 
-drush language-import-translations de ../language/alpha4.po --replace --groups=default
+drush language-import-translations de ../language/alpha5.po --replace --groups=default
 
 
 #drush vset maintenance_mode 0
