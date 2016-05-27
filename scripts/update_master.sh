69fc4c45776eb61aa13ceddd85543d958b765fbc
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2016-05-27 14:51:46 +0200

    update master

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -1,22 +1,25 @@
 ##!/bin/sh
+drush vset maintenance_mode 1
+
+drush privatize --yey
+
 drush fr  videosafe_features --yes
 drush en videosafe --yes
 mkdir -p ../files_private/videosafe
 chmod -R 0777  ../files_private
 
-drush vset maintenance_mode 1
 drush fr nm_general_features --yes
 drush fr nm_section_courses_features --yes
 drush fr nm_section_content_features --yes
 drush fr section_projects_features --yes
-drush fr nm_h5p_features --yes 
+drush fr nm_h5p_features --yes
 drush fr  videosafe_features --yes
-drush en privatemsg
-drush en media_wysiwyg
+drush en privatemsg --yes
+drush en media_wysiwyg --yes
 
 
 echo -e  "RewriteEngine on \nRewriteRule ^content/([0-9]+)/videos/(.*)$ /system/files/videosafe/\$2 [R=307,L]\nRewriteRule ^content/([0-9]+)/videosafe/(.*)$ /system/files/videosafe/\$2 [R=301,L]" > sites/default/files/h5p/.htaccess
- 
+
 find sites/default/files/h5p -type f -iname "files-*.*" -exec  cp {}  ../files_private/videosafe/  \;
 
 
