a23f59f3924819dc0297d6ecad10d76c4694c073
Bernhard Strehl <bernhard.strehl@phil.uni-augsburg.de>
2016-05-30 13:42:02 +0200

    videosafe: ausfuehrreihenfolge update angepasst

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -6,6 +6,16 @@ chmod -R 0777  ../files_private
 
 drush vset maintenance_mode 1
 drush fr nm_general_features --yes
+drush en  videosafe_features --yes
+drush fr  videosafe_features --yes
+echo   "RewriteEngine on \nRewriteRule ^content/([0-9]+)/videos/(.*)$ /system/files/videosafe/\$2 [R=307,L]\nRewriteRule ^content/([0-9]+)/videosafe/(.*)$ /system/files/videosafe/\$2 [R=301,L]" > sites/default/files/h5p/.htaccess
+ 
+find sites/default/files/h5p -type f -iname "files-*.*" -exec  cp {}  ../files_private/videosafe/  \;
+
+drush en videosafe --yes
+
+
+drush fr nm_general_features --yes
 drush fr nm_section_courses_features --yes
 drush fr nm_section_content_features --yes
 drush fr section_projects_features --yes
@@ -15,9 +25,6 @@ drush en privatemsg
 drush en media_wysiwyg
 
 
-echo -e  "RewriteEngine on \nRewriteRule ^content/([0-9]+)/videos/(.*)$ /system/files/videosafe/\$2 [R=307,L]\nRewriteRule ^content/([0-9]+)/videosafe/(.*)$ /system/files/videosafe/\$2 [R=301,L]" > sites/default/files/h5p/.htaccess
- 
-find sites/default/files/h5p -type f -iname "files-*.*" -exec  cp {}  ../files_private/videosafe/  \;
 
 
 #clear cache
