dcb5d7765948d94e6b277a20808b7da4d0a1515b
Bernhard Strehl <bernhard.strehl@phil.uni-augsburg.de>
2016-06-30 14:39:08 +0200

    videosafe: neue htaccess-anpassung (404 bilder fix)

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -17,8 +17,8 @@ drush en media_wysiwyg --yes
 drush fr nm_general_features --yes
 drush en  videosafe_features --yes
 drush fr  videosafe_features --yes
-echo   "#verweise auf bilder etc falls in private\nRewriteEngine on\nRewriteCond %{REQUEST_FILENAME} !-f\nRewriteCond %{REQUEST_FILENAME} !videosafe\nRewriteRule content/([0-9]+)/(\w*)/(files-.*)$ /system/files/h5p/content/\$1/\$2/\$3 [R=301,L]\n#videosafe immer in private\nRewriteEngine on \nRewriteCond %{REQUEST_URI} videosafe\nRewriteRule ^content/([0-9]+)/videosafe/(.*)$ /system/files/videosafe/\$2 [R=301,L]\n" > sites/default/files/h5p/.htaccess
-echo   "RewriteEngine on\nRewriteRule ^videosafe/(.*)$ /system/files/videosafe/\$1 [R=307,L]\n#videos aus privatem dateisystem laden\nRewriteEngine on\nRewriteCond %{REQUEST_FILENAME} !-f\nRewriteCond %{REQUEST_FILENAME} !videosafe\nRewriteRule content/([0-9]+)/(\w*)/(files-.*)$ /system/files/h5p/content/\$1/\$2/\$3 [R=307,L]"  > sites/default/files/h5peditor/.htaccess
+echo   "#verweise auf bilder etc falls in private\nRewriteEngine on\nRewriteCond %{REQUEST_FILENAME} !-f\nRewriteCond %{REQUEST_FILENAME} !videosafe\nRewriteRule RewriteRule content/([0-9]+)/(\w*)/([a-zA-Z\-\.0-9]*)$ /system/files/h5p/content/\$1/\$2/\$3 [R=301,L]\n#videosafe immer in private\nRewriteEngine on \nRewriteCond %{REQUEST_URI} videosafe\nRewriteRule ^content/([0-9]+)/videosafe/(.*)$ /system/files/videosafe/\$2 [R=301,L]\n" > sites/default/files/h5p/.htaccess
+echo   "RewriteEngine on\nRewriteRule ^videosafe/(.*)$ /system/files/videosafe/\$1 [R=307,L]\n#videos aus privatem dateisystem laden\nRewriteEngine on\nRewriteCond %{REQUEST_FILENAME} !-f\nRewriteCond %{REQUEST_FILENAME} !videosafe\nRewriteRule content/([0-9]+)/(\w*)/(.*)$ /system/files/h5p/content/\$1/\$2/\$3 [R=307,L]"  > sites/default/files/h5peditor/.htaccess
 #find sites/default/files/h5p -type f -iname "files-*.*" -exec  cp {}  ../files_private/videosafe/  \;
 drush en videosafe --yes
 
