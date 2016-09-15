d03ea876fbd90776b6e1efdd45dffd68179f637c
Bernhard Strehl <bernhard.strehl@phil.uni-augsburg.de>
2016-09-15 12:22:10 +0200

    oembed_fix: nicht mehr notwendig

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -4,7 +4,7 @@ drush vset maintenance_mode 1
 drush en linkit_extension --yes
 
 drush fr nm_section_content_features --yes
-
+drush dis oembed_fix  --yes
 #clear cache
 drush cc all
 drush updatedb --yes
