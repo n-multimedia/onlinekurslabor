653af490b9189ea74eb9648b3f9f442de2f02302
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2016-11-04 15:01:20 +0100

    alpha7 structure

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -1,23 +1,13 @@
 ##!/bin/sh
 drush vset maintenance_mode 1
 
-drush en linkit_extension --yes
-drush en bootstrap_check --yes
-drush en views_php --yes
-drush pm-disable bartik --yes
-drush pm-disable seven  --yes
 
-drush fr nm_section_content_features nm_general_features nm_h5p_features nm_section_content_features nm_section_courses_features section_courses_clone_features section_projects_features --yes
-drush dis oembed_fix  --yes
 #clear cache
 drush cc all
 drush updatedb --yes
-drush fr nm_h5p_features nm_stream_features --yes
-
-drush php-eval 'node_access_rebuild();'
 
 
-drush language-import-translations de ../language/alpha6.po --replace --groups=default
+drush language-import-translations de ../language/alpha7.po --replace --groups=default
 
 
 drush vset maintenance_mode 0
