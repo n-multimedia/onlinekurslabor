d8102bda777be06ccf6bd7e9b2dabe1bca969485
Bernhard Strehl <bernhard.strehl@phil.uni-augsburg.de>
2016-11-10 15:50:35 +0100

    insta_test: commit

---
+++ scripts/insta_update.sh
@@ -0,0 +1,26 @@
+##!/bin/bash
+cd "$(dirname "$0")"
+
+# zeit seit letztem push
+#currdate=`date +%s`
+#gitdate=`git log -1 --pretty=format:%ct`
+#datediff=`expr $currdate - $gitdate`
+
+#hat sich im github was geandert?
+changed_dev=`git remote update && git status -uno | grep "Your branch is behind"` #alternativ git diff origin/dev
+
+if [ "$changed_dev" != "Fetching origin" ];
+then 
+	git pull
+	#path von drush ohne systeminstallation benoetigt
+	export PATH=$PATH:~/drush/
+	cd ../web/
+	#leere log und setze start
+	echo "RUNNING UPDATE" > sites/default/files/last_update_log
+	sh ../scripts/update_master.sh &>>  sites/default/files/last_update_log
+	echo "" >> sites/default/files/last_update_log
+	echo "Update completed on:" >> sites/default/files/last_update_log
+	date >> sites/default/files/last_update_log
+else
+	echo "nothing changed."
+fi
