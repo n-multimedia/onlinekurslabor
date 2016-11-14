3eb53632c326ba35a12a11d0a0da2a24e5a1516b
Bernhard Strehl <bernhard.strehl@phil.uni-augsburg.de>
2016-11-14 12:58:51 +0100

    ie11: zeige warning

---
+++ web/sites/all/modules/onlinekurslabor/browser_compatibility/assets/browser_compatibility.js
@@ -0,0 +1,29 @@
+// A $( document ).ready() block.
+jQuery( document ).ready(function() {
+    if(browser_compatibility_getIEVersion() === 11)
+    {
+        jQuery(".browser_compatibility_custom_message").show();
+        jQuery(".browser_compatibility_warning_affected_browser").html("Internet Explorer 11");
+        jQuery(".browser_compatibility_warning_details").html("<ul><li>Das Bearbeiten von Text funktioniert nicht vollständig.</li><li>Sie werden Probleme beim Abspielen von Videos haben.</li></ul>")
+    }
+});
+
+/**
+ * checks if ie
+ * @returns {Number} ie-version or 0 if not ie
+ */
+function browser_compatibility_getIEVersion() {
+  var sAgent = window.navigator.userAgent;
+  var Idx = sAgent.indexOf("MSIE");
+
+  // If IE, return version number.
+  if (Idx > 0) 
+    return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+
+  // If IE 11 then look for Updated user agent string.
+  else if (!!navigator.userAgent.match(/Trident\/7\./)) 
+    return 11;
+
+  else
+    return 0; //It is not IE
+}
