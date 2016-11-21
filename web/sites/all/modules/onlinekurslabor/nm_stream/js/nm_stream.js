62bfb6827b2a5f9561e3607a74ca8e616e8dce2c
Bernhard Strehl <bernhard.strehl@phil.uni-augsburg.de>
2016-11-21 16:11:43 +0100

    stream: fixe: verhalten mit doppelten html-ids

--- web/sites/all/modules/onlinekurslabor/nm_stream/js/nm_stream.js
+++ web/sites/all/modules/onlinekurslabor/nm_stream/js/nm_stream.js
@@ -742,10 +742,10 @@
 
                             } else {
 
-                                //append new node
-                                var new_node = post_button.closest('.nm-stream-node-container').before($(data.node).fadeIn());
+                                //remove old node, append new node
+                                post_button.closest('.nm-stream-node-container').remove().before(data.node).fadeIn();
 
-                                post_button.closest('.nm-stream-node-container').hide();
+                                var new_node = post_button.closest('.nm-stream-node-container');
                                 //attach behavior
                                 Drupal.attachBehaviors(new_node);
                             }
