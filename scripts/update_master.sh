1a6c87801ceed79dbd96ba949f69a79200940153
Bernhard Strehl <bernhard.strehl@phil.uni-augsburg.de>
2016-11-29 14:09:23 +0100

    annvid: og_group-problem: fixed

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -11,7 +11,7 @@ drush updatedb --yes
 drush en browser_compatibility --yes
 #bug in drush - cc before FR!!
 drush cc all
-drush fr  videosafe_features  nm_section_courses_features  section_projects_features nm_h5p_features --yes
+drush fr  videosafe_features  nm_section_courses_features  section_projects_features nm_h5p_features annvid_features --yes
 
 
 drush language-import-translations de ../language/alpha7.po --replace --groups=default
