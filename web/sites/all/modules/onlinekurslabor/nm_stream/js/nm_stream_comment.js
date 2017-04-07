2f87dfa40c6ef9be45ec898ac77a85c553d8d8d1
Sergej Naumenko <sergej.naumenko@phil.uni-augsburg.de>
2017-04-07 10:54:45 +0200

    nm stream refactoring part 1 - snapshot

---
+++ web/sites/all/modules/onlinekurslabor/nm_stream/js/nm_stream_comment.js
@@ -0,0 +1,56 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function($) {
+
+    /**
+     * constructor
+     *
+     * @param context
+     * @constructor
+     */
+    function NMStreamComment(context) {
+        this.state = 0;
+        this.container = context;
+    }
+
+    /**
+     * bind events
+     */
+    NMStreamComment.prototype.init_bind_events = function() {
+
+    };
+
+    /**
+     * add
+     */
+    NMStreamComment.prototype.add = function() {
+
+    };
+
+    /**
+     * update
+     */
+    NMStreamComment.prototype.update = function() {
+
+    };
+
+    /**
+     * delete
+     */
+    NMStreamComment.prototype.delete = function() {
+
+    };
+
+    /**
+     * delete
+     */
+    NMStreamComment.prototype.refresh = function() {
+
+    };
+
+
+
+}(jQuery));
---
+++ web/sites/all/modules/onlinekurslabor/nm_stream/js/nm_stream_node.js
@@ -0,0 +1,56 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function($) {
+
+    /**
+     * constructor
+     *
+     * @param context
+     * @constructor
+     */
+    function NMStreamNode(context) {
+        this.state = 0;
+        this.container = context;
+    }
+
+    /**
+     * bind events
+     */
+    NMStreamNode.prototype.init_bind_events = function() {
+
+    };
+
+    /**
+     * add
+     */
+    NMStreamNode.prototype.add = function() {
+
+    };
+
+    /**
+     * update
+     */
+    NMStreamNode.prototype.update = function() {
+
+    };
+
+    /**
+     * delete
+     */
+    NMStreamNode.prototype.delete = function() {
+
+    };
+
+    /**
+     * delete
+     */
+    NMStreamNode.prototype.refresh = function() {
+
+    };
+
+
+
+}(jQuery));
