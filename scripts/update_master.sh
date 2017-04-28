34119b03770a3e99b01883ac1016cd1f0f1ff095
Bernhard Strehl <bernhard.strehl@phil.uni-augsburg.de>
2017-04-28 14:32:18 +0200

    alpha8: initial commit

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -1,23 +1,16 @@
 ##!/bin/sh
 drush vset maintenance_mode 1
-
-wget -O sites/default/files/h5p/libraries/H5PEditor.InteractiveVideo-1.15/Scripts/interactive-video-editor.js https://trello-attachments.s3.amazonaws.com/559fa59ca3cc5213d6d8b07d/58e215a8bf2256697570d093/631743669be449f18d955a4909fc7220/interactive-video-editor.js
-#clear cache
+ 
 drush cc all
-
-drush dis overlay --yes
-
-drush updatedb --yes
-drush en browser_compatibility --yes
-drush en no_mailer --yes
-drush en annvid --yes
-
+ 
+drush updatedb --yes 
 #bug in drush - cc before FR!!
 drush cc all
-drush features-revert-all --yes
+#revert single features
+#drush features-revert-all --yes
 
 
-drush language-import-translations de ../language/alpha7.po --replace --groups=default
+drush language-import-translations de ../language/alpha8.po --replace --groups=default
 
 
 drush vset maintenance_mode 0
