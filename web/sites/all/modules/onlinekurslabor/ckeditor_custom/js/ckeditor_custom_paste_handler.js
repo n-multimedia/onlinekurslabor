cca7eddf10dbe7bf9351baaaf86191d3e30da5f0
null <null@users.noreply.github.com>
2017-05-04 13:57:59 +0200

    ckeditor_custom: fix js-error

--- web/sites/all/modules/onlinekurslabor/ckeditor_custom/js/ckeditor_custom_paste_handler.js
+++ web/sites/all/modules/onlinekurslabor/ckeditor_custom/js/ckeditor_custom_paste_handler.js
@@ -1,22 +1,28 @@
 /*
  * Script ersetzt img-Tags durch Hinweistext
  */
-// A $( document ).ready() block.
+
 jQuery(document).ready(function() {
 
     var ckinstances_list = CKEDITOR.instances;
-//get first index in element-list
-    var first_ck_index = Object.keys(ckinstances_list)[0];
-    var the_ck_editor = ckinstances_list[first_ck_index];
+    //falls CKEditor geladen. Wenn nicht skippen, sonst JS-Fehler
+    if (Object.keys(ckinstances_list).length > 0)
+    {
 
-    the_ck_editor.on('paste', function(evt) {
-        //sonst doppeltes einfügen
-        evt.stop();
-        // replace img-tag by alternative image
-        var data = evt.data.dataValue.replace(/<img( [^>]*)?>/gi, '<img src="'+Drupal.settings.ckeditor_custom.default_image+'" alt="Bilder bitte hochladen">');
-        evt.editor.insertHtml(data);
+        //get first index in element-list
+        var first_ck_index = Object.keys(ckinstances_list)[0];
+        alert(first_ck_index);
+        var the_ck_editor = ckinstances_list[first_ck_index];
 
-    }, the_ck_editor.element.$);
+        the_ck_editor.on('paste', function(evt) {
+            //sonst doppeltes einfügen
+            evt.stop();
+            // replace img-tag by alternative image
+            var data = evt.data.dataValue.replace(/<img( [^>]*)?>/gi, '<img src="' + Drupal.settings.ckeditor_custom.default_image + '" alt="Bilder bitte hochladen">');
+            evt.editor.insertHtml(data);
 
+        }, the_ck_editor.element.$);
+
+    }
 
 });
