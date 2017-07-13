519aba12bf18100b506f94c9e122882ffdfdb951
null <null@users.noreply.github.com>
2017-07-13 15:15:57 +0200

    profile: data + visibilites

--- web/sites/all/modules/onlinekurslabor/custom_general/modules/section_profile/templates/profile-details.tpl.php
+++ web/sites/all/modules/onlinekurslabor/custom_general/modules/section_profile/templates/profile-details.tpl.php
@@ -1,26 +1,23 @@
 <?php
+
 /*
  * Tabelle mit User-Details im Profil
  */
 $user = user_load($uid);
-?>
- 
-<div class="row" >
-    <div class="col-md-12"  id="section_profile-detailinfos">
+//übergebener parameter $range
+$range;
 
 
-        <table width="100%;">
-            <? foreach (_section_profile_get_profile_data($user) as $title => $info): ?>
-                <tr>
-                    <td width="35%;" valign="top">
-                        <?= $title ?>
-                    </td>
-                    <td width="65%;"   align="right">
-                        <?= $info ?>
-                    </td>
-                </tr>
-            <? endforeach; ?>
+$profile_data = _section_profile_get_profile_data($user) [$range];
+//überschrift
+$table_header = array(array('data' => t(ucfirst($range) . ' information'), 'colspan' => 2));
+$row_content = array();
+//löst profil-daten in theme_table-variablen auf https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7.x
+foreach ($profile_data as $title => $info) {
+    $row_content[] = array('data' => array(
+            array('data' => $title, 'width' => '35%'), array('data' => $info, 'width' => '65%', 'align' => 'right')
+    ));
+}
+echo theme('table', array('header' => $table_header, 'rows' => $row_content, 'attributes' => array('class'=>'profile_details_table_content_wrap')));
 
-        </table>
-    </div>
-</div>
+
