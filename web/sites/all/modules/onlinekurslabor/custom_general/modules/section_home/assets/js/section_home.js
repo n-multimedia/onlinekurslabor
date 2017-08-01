cb823238775f4780e98380841870b2aa2b0f9516
null <null@users.noreply.github.com>
2017-08-01 12:16:55 +0200

    home: searchbox: ajax-behaviour fuer "alle" statt Reset

--- web/sites/all/modules/onlinekurslabor/custom_general/modules/section_home/assets/js/section_home.js
+++ web/sites/all/modules/onlinekurslabor/custom_general/modules/section_home/assets/js/section_home.js
@@ -1,5 +1,7 @@
 /**
- * Ändert Verhalten des Form-Reset-Buttons auf /home in der Kurssuche
+ * Aktiviert collapse-Verhalten bei searchform auf /home in der Kurssuche
+ * und
+ * Ändert Verhalten des Form-Reset-Buttons ("Alle")
  */
 
 
@@ -8,9 +10,17 @@
     Drupal.behaviors.section_home = {
         //attach wird bei jedem nachladen aufgerufen
         attach: function(context, settings) {
+            var searchbox_form = $("form#views-exposed-form-courses-kurse-bersicht-my-courses-small");
             /*add toggle-behaviour to searchform*/
             //aktiviere searchform bei ajax
-            $("form#views-exposed-form-courses-kurse-bersicht-my-courses-small").addClass("collapse in");
+            $(searchbox_form).addClass("collapse in");
+
+            //neues behaviour für reset-button("alle")
+            $("button#edit-reset", searchbox_form).click(function() {
+                $("input#edit-course-title", searchbox_form).val("");
+                $("button#edit-submit-courses-kurse-bersicht", searchbox_form).click();
+                return false;
+            });
         }
     };
 
