a2cd18d2d1337d15b02fc31ed0f8ceebba8dad23
null <null@users.noreply.github.com>
2017-08-01 11:47:46 +0200

    home: searchbox: data-toggle2

--- web/sites/all/modules/onlinekurslabor/custom_general/modules/section_home/assets/js/section_home.js
+++ web/sites/all/modules/onlinekurslabor/custom_general/modules/section_home/assets/js/section_home.js
@@ -8,15 +8,17 @@
     Drupal.behaviors.section_home = {
         //attach wird bei jedem nachladen aufgerufen
         attach: function(context, settings) {
-
+            /*add toggle-behaviour to searchform*/
+            //aktiviere searchform bei ajax
+            $("form#views-exposed-form-courses-kurse-bersicht-my-courses-small").addClass("collapse in");
         }
     };
 
     //außerhalb drupal-scope
     // nur einmalig beim Laden der Seite soll die searchform versteckt werden
     $(document).ready(function() {
-        /*add toggle-behaviour to searchform*/
-        $("form#views-exposed-form-courses-kurse-bersicht-my-courses-small").addClass("collapse");
+        //initial form ausblenden
+        $("form#views-exposed-form-courses-kurse-bersicht-my-courses-small").removeClass("in");
     });
 
 }(jQuery));
