0484fb6617c905ab27ffeb0186dbeb5dd73f7b02
null <null@users.noreply.github.com>
2017-09-22 15:26:07 +0200

    alpha8: last commit

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -7,30 +7,15 @@ drush updatedb --yes
 #bug in drush - cc before FR!!
 drush cc all
 #revert single features
-drush fr  nm_general_features nm_login_vhb_features home_features  nm_section_content_features nm_section_courses_features  section_courses_clone_features nm_h5p_features annvid_features nm_section_content_features  nm_stream_features  section_projects_features notification_features  --yes
+
 #drush features-revert-all --yes
 
  
 
-drush en mediamodule_fix --yes
-drush en ckeditor_custom --yes
-drush en home_features --yes
-drush en section_home --yes
-drush en changediscard_warning --yes
-
-
-
-drush en section_profile --yes
-
-drush en bootstrap_check --yes
-
 
 #dis
-drush dis browser_compatibility --yes
-drush dis og_field_access --yes
-drush dis statuses --yes
 
-drush language-import-translations de ../language/alpha8.po --replace --groups=default
+drush language-import-translations de ../language/alpha9.po --replace --groups=default
 
 
 
