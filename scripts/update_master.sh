6be6c5611b553f84092c1b9b3dbf077f44196060
Sergej Naumenko <office@naumenko-multimedia.de>
2017-10-20 13:12:25 +0200

    moderator page and views prepared

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -2,17 +2,25 @@
 drush vset maintenance_mode 1
  
 drush cc all
- 
-drush updatedb --yes 
+
+drush updatedb --yes
 #bug in drush - cc before FR!!
 drush cc all
 #revert single features
-#drush fr  section_projects_features/  --yes
-#drush features-revert-all --yes
+
 
 drush en nm_administration --yes
 
+#drush features-revert-all --yes
+
+
+drush fr nm_administration --yes
+
+
+#dis
+
 drush language-import-translations de ../language/alpha9.po --replace --groups=default
 
+drush cc all
 
 drush vset maintenance_mode 0
