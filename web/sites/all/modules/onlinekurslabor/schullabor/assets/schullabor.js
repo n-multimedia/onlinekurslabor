5f7e386cbf59468b442978524b162736e8aed55b
null <null@users.noreply.github.com>
2017-11-27 11:35:21 +0100

    home: osl-start umgesetzt

--- web/sites/all/modules/onlinekurslabor/schullabor/assets/schullabor.js
+++
@@ -1,7 +0,0 @@
-
-/*
- * ersetzt description auf der startseite
- */
-jQuery(document).ready(function() {
-    jQuery("div.navbar-header a.navbar-brand img").attr("src",Drupal.settings.schullabor.base_path+"/assets/images/schullabor-logo.png").css("background-repeat","no-repeat");
-});
