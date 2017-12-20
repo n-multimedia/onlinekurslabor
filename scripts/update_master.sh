b06c064418ca41e6da53312977bdd85dbf27af86
Sergej Naumenko <office@naumenko-multimedia.de>
2017-12-20 15:38:41 +0100

    update master

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -9,7 +9,7 @@ drush cc all
 drush en nm_administration --yes
 drush en help_features --yes
 #revert single features
-drush fr nm_section_content_features nm_section_courses_features nm_general_features section_projects_features --yes
+drush fr nm_section_content_features nm_section_courses_features nm_general_features section_projects_features nm_h5p_features --yes
 #drush features-revert-all --yes
  
 
