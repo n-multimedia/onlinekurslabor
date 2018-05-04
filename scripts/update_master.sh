3234fdbe32e12be4331943832de27260ad5a39b6
null <null@users.noreply.github.com>
2018-05-04 11:16:18 +0200

    update_master: add missing FR

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -14,7 +14,8 @@ drush en nm_administration --yes
 #revert single features
 
 #drush fr nm_section_content_features nm_section_courses_features nm_general_features section_projects_features nm_h5p_features section_courses_clone_features --yes
-drush fr nm_section_courses_features nm_administration --yes
+drush fr nm_section_courses_features nm_administration  nm_section_content_features --yes
+drush cc all
 
 drush en section_administration --yes
 drush en  og_field_fix --yes
