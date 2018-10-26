3ccaedc02f0aff0344e1da5816407fa2d5cbe10d
null <null@users.noreply.github.com>
2018-10-26 09:44:15 +0200

    wartungsscript optimiert - emails via plesk-cron

--- scripts/default.emergency_mail_contact.sh
+++
@@ -1,3 +0,0 @@
-##!/bin/bash
-#name the recipients for emergency-mail-contact in case the system gets switched off (seperated by comma)
-EMERGENCY_MAIL="VALIDADRESS@PROVIDER"
--- scripts/emergency_mail_contact.sh
+++
@@ -1,3 +0,0 @@
-##!/bin/bash
-EMERGENCY_MAIL="support@medienlabor-uni-augsburg.de,bernhard.strehl@gmail.com"
-#EMERGENCY_MAIL="support@medienlabor-uni-augsburg.de"
