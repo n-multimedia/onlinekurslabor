d8f48745df076e0b90a56e52741d2ee9121eca82
Sergej Naumenko <office@naumenko-multimedia.de>
2019-03-29 15:21:02 +0100

    update script

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ##!/bin/sh
 drush vset maintenance_mode 1
- 
+
 
 #bug in drush - cc before FR!!
 drush cc all
@@ -11,11 +11,11 @@ drush en realname_fix --yes
 
 #revert single features
 
-drush fr  nm_section_courses_features --y
+drush fr  nm_section_courses_features annvid_features --y
 #drush fr nm_section_content_features nm_section_courses_features nm_general_features section_projects_features nm_h5p_features section_courses_clone_features --yes
 
 drush cc all
-  
+
 
 
 #import language
