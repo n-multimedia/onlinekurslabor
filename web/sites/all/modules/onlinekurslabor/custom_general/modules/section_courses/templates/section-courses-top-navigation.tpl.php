582bedfbcb8398f08e76ae7f81ff9fefc3635a4b
Sergej Naumenko <office@naumenko-multimedia.de>
2019-05-31 13:42:10 +0200

    initial responsive menue

---
+++ web/sites/all/modules/onlinekurslabor/custom_general/modules/section_courses/templates/section-courses-top-navigation.tpl.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * User: naumenko-multimedia
+ * Date: 17.05.2019
+ * Time: 17:16
+ */
+
+?>
+
+<nav class="navbar navbar-default section_navigation_course"
+     id="section_navigation">
+  <div class="container-fluid">
+    <div class="col-md-6">
+      <div class="navbar-header">
+        <div id="course_top_nav_single-icon"></div>
+      </div>
+      <ul class="nav navbar-nav okl-nav-course-bar">
+        <?php foreach ($single_links as $link): ?>
+          <?php print $link['rendered_link']; ?>
+        <?php endforeach; ?>
+      </ul>
+    </div>
+    <div class="col-md-6">
+      <div class="navbar-header">
+        <div id="course_top_nav_collab-icon"></div>
+      </div>
+      <ul class="nav navbar-nav okl-nav-course-bar">
+        <?php foreach ($collab_links as $link): ?>
+          <?php print $link['rendered_link']; ?>
+        <?php endforeach; ?>
+      </ul>
+    </div>
+  </div>
+</nav>
+<?php if (!empty($admin_tools)): ?>
+  <nav class="navbar navbar-inverse" id="section-nav-instructor-tools">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                data-target="#instructor-tools">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="instructor-tools">
+        <?php print $admin_tools; ?>
+      </div>
+    </div>
+  </nav>
+<?php endif; ?>
+
