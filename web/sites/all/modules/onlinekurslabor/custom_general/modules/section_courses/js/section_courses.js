d342038d5886e3304c6e0cc79299e543ed5c37e0
null <null@users.noreply.github.com>
2019-07-17 14:59:04 +0200

    section_courses: klappmenue-js-fixes

--- web/sites/all/modules/onlinekurslabor/custom_general/modules/section_courses/js/section_courses.js
+++ web/sites/all/modules/onlinekurslabor/custom_general/modules/section_courses/js/section_courses.js
@@ -1,31 +1,28 @@
 (function ($) {
+    
     Drupal.behaviors.section_courses = {
         attach: function (context, settings) {
-            //attach funktinoiert nicht in subclasses
-            Drupal.behaviors.section_courses.tasks.removeEmptyTaskFields();
+           //....
         }
     };
 
-
-    Drupal.behaviors.section_courses.tasks = {
-        /*remove new empty fields when editing a task*/
-        removeEmptyTaskFields: function () {
-            //wird in php gesetzt
-            var task_empty_fields = Drupal.settings.section_courses.task_remove_empty_fields;
-            $('#field-generic-task-entry-values .double-field-elements button').once('section_courses_tasks', function (index, context) {
-
-                if (Array.isArray(task_empty_fields) && task_empty_fields.indexOf(index) >= 0) {
-                    //$(this).trigger('click');
-                    var that = this;
-                    
-                    //gewisse verzoegerung, sonst probleme mit ckeditor
-                    setTimeout(function () {
-                        $(that).trigger('mousedown').closest('tr').hide();
-                    }, 1300);
-                }
-
+    
+    Drupal.behaviors.section_courses.menu = {
+        //die Links im Kursmenü haben ein default-Ziel. der Link soll nicht auf eine andere Seite führen, wenn wir ins Menü klicken.
+        stopLinkExecutionForMenuentries: function ()
+        {
+            $("div#instructor-tools li[data-toggle=collapse]").click(function (e)
+            {
+                event.preventDefault();
             });
         }
     };
 
 }(jQuery));
+
+
+
+jQuery(document).ready(function() {
+    Drupal.behaviors.section_courses.menu.stopLinkExecutionForMenuentries();
+});
+
---
+++ web/sites/all/modules/onlinekurslabor/custom_general/modules/section_courses/js/section_courses_task.js
@@ -0,0 +1,23 @@
+(function ($) {
+    Drupal.behaviors.section_courses_task = {
+        /*remove new empty fields when editing a task*/
+        attach: function (context, settings) {
+            //wird in php gesetzt
+            var task_empty_fields = Drupal.settings.section_courses.task_remove_empty_fields;
+            $('#field-generic-task-entry-values .double-field-elements button').once('section_courses_tasks', function (index, context) {
+
+                if (Array.isArray(task_empty_fields) && task_empty_fields.indexOf(index) >= 0) {
+                    //$(this).trigger('click');
+                    var that = this;
+
+                    //gewisse verzoegerung, sonst probleme mit ckeditor
+                    setTimeout(function () {
+                        $(that).trigger('mousedown').closest('tr').hide();
+                    }, 1300);
+                }
+
+            });
+        }
+    };
+
+}(jQuery));
