001c1f9cfc4a519b71e2bd1d075c55950334e8b8
null <null@users.noreply.github.com>
2019-07-17 11:38:13 +0200

    section_courses: refactorJS II

--- web/sites/all/modules/onlinekurslabor/custom_general/modules/section_courses/js/section_courses.js
+++ web/sites/all/modules/onlinekurslabor/custom_general/modules/section_courses/js/section_courses.js
@@ -1,21 +1,30 @@
-(function($) {
+(function ($) {
+    Drupal.behaviors.section_courses = {
+        attach: function (context, settings) {
+            //attach funktinoiert nicht in subclasses
+            Drupal.behaviors.section_courses.tasks.removeEmptyTaskFields();
+        }
+    };
 
-    Drupal.behaviors.section_courses_tasks = {
-        /*toggle solution for qaa questions*/
-        attach: function(context, settings) {
 
-          var task_empty_fields = Drupal.settings.section_courses.task_remove_empty_fields;
+    Drupal.behaviors.section_courses.tasks = {
+        /*remove new empty fields when editing a task*/
+        removeEmptyTaskFields: function () {
+            //wird in php gesetzt
+            var task_empty_fields = Drupal.settings.section_courses.task_remove_empty_fields;
+            $('#field-generic-task-entry-values .double-field-elements button').once('section_courses_tasks', function (index, context) {
 
-          $('#field-generic-task-entry-values .double-field-elements button').once('section_courses_tasks', function(index, context){
+                if (Array.isArray(task_empty_fields) && task_empty_fields.indexOf(index) >= 0) {
+                    //$(this).trigger('click');
+                    var that = this;
+                    
+                    //gewisse verzoegerung, sonst probleme mit ckeditor
+                    setTimeout(function () {
+                        $(that).trigger('mousedown').closest('tr').hide();
+                    }, 1300);
+                }
 
-            if(Array.isArray(task_empty_fields) && task_empty_fields.indexOf(index) >= 0){
-              //$(this).trigger('click');
-              var that = this;
-              //gewisse verzoegerung, sonst probleme mit ckeditor
-              setTimeout(function(){  $(that).trigger('mousedown').closest('tr').hide();},1300);
-            }
-
-          });
+            });
         }
     };
 
