012bc2c11f5d6c70c9dd9612bc3f86f86f6941c1
Sergej Naumenko <office@naumenko-multimedia.de>
2019-07-19 15:48:26 +0200

    panels without instructors menu

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -10,9 +10,9 @@ drush cc all
 
 #revert single features
 
-
+drush fr nm_section_courses_features --yes
 #drush fr nm_section_content_features nm_section_courses_features nm_general_features section_projects_features nm_h5p_features section_courses_clone_features --yes
-  
+
 #import language
 drush language-import-translations de ../language/alpha14.po --replace --groups=default
 
