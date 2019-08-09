f4bcfc58691a5a365d8f89a840583499d302810b
Sergej Naumenko <office@naumenko-multimedia.de>
2019-08-09 12:56:32 +0200

    content mobile menu

--- web/sites/all/modules/onlinekurslabor/custom_general/modules/section_content/js/section_content.js
+++ web/sites/all/modules/onlinekurslabor/custom_general/modules/section_content/js/section_content.js
@@ -148,3 +148,32 @@
   };
 
 }(jQuery));
+
+
+(function ($) {
+
+  Drupal.behaviors.section_content = {
+    attach: function (context, settings) {
+      //....
+    }
+  };
+
+  Drupal.behaviors.section_content.menu = {
+    //die Links im Autorenmenue haben ein default-Ziel. der Link soll nicht auf eine andere Seite führen, wenn wir ins Menü klicken.
+    stopLinkExecutionForMenuentries: function ()
+    {
+      $("div#authoring-tools li[data-toggle=collapse]").click(function (e)
+      {
+        event.preventDefault();
+      });
+    }
+  };
+
+}(jQuery));
+
+
+
+jQuery(document).ready(function() {
+  Drupal.behaviors.section_content.menu.stopLinkExecutionForMenuentries();
+});
+
