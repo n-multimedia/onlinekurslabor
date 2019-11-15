df3fcfb9254c59b7e1bc4c114eed2bd39d4651d4
Sergej Naumenko <office@naumenko-multimedia.de>
2019-11-15 13:16:30 +0100

    added missing permission to feature

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -9,13 +9,13 @@ drush cc all
 
 
 #revert single features
-drush fr notification_features nm_section_courses_features videosafe_features nm_section_content_features --yes
+drush fr notification_features nm_section_courses_features videosafe_features nm_section_content_features annvid_features --yes
 #drush fr nm_section_content_features nm_section_courses_features nm_general_features section_projects_features nm_h5p_features section_courses_clone_features --yes
 
 #import language
 drush language-import-translations de ../language/alpha15.po --replace --groups=default
 
-drush updatedb --yes 
+drush updatedb --yes
 
 drush pm-disable newmenue_tooltip --y
 drush pm-uninstall newmenue_tooltip --y
