3b216bee5cf24851987c0c9f39268c1f8578ab7c
null <null@users.noreply.github.com>
2020-09-11 11:04:59 +0200

    alpha18: initial commit

--- scripts/update_alpha17.sh
+++ scripts/update_alpha17.sh
@@ -10,14 +10,21 @@ drush vset maintenance_mode 1
 drush cc all
 
 #revert single features
-#drush fr     --yes
+drush fr    nm_section_content_features nm_general_features nm_section_courses_features notification_features nm_administration --yes
 #drush fr annvid_features nm_section_content_features nm_section_courses_features nm_general_features section_projects_features nm_h5p_features section_courses_clone_features --yes
 
 #import language
-drush language-import-translations de ../language/alpha18.po --replace --groups=default
+drush language-import-translations de ../language/alpha17.po --replace --groups=default
 
 drush updatedb --yes
 
 drush cc all
 
 drush vset maintenance_mode 0
+drush vset maintenance_mode 1
+
+drush @none dl utf8mb4_convert-7.x --y
+drush cc drush
+drush utf8mb4-convert-databases --y
+
+echo "Now fix your config. Enter drush vset maintenance_mode 0 afterwards."
