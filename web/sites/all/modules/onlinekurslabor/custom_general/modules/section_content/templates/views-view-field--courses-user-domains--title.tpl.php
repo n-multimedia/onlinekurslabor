2d38c770996cec8a788a8e6a8c50f561f24eb94c
null <null@users.noreply.github.com>
2020-10-07 13:12:07 +0200

    Lehrtextmitgliederverwaltung: RE: 32ef656013bb0c482dd922638ffb02490b788ed3 fixes badge on /user/uid

--- web/sites/all/modules/onlinekurslabor/custom_general/modules/section_content/templates/views-view-field--courses-user-domains--title.tpl.php
+++ web/sites/all/modules/onlinekurslabor/custom_general/modules/section_content/templates/views-view-field--courses-user-domains--title.tpl.php
@@ -1,10 +1,19 @@
 <?php
 /**
- * FILE for link to LEHRTEXT on /domains
+ * FILE for link to LEHRTEXT on /domains / profile / HOME
  */
 //dpm($row);
+global $user;
+//if we're viewing a different user
+$affected_user_uid = $view->argument['uid']->argument;
+$user_for_state = $user;
+if($affected_user_uid)
+{
+  $user_for_state = user_load($affected_user_uid);
+}
+
 $node = node_load($row->nid);
-$users_state_for_domain = _custom_general_get_group_state_for_user($node);
+$users_state_for_domain = _custom_general_get_group_state_for_user($node,$user_for_state);
 ?>
 
 <?php if($users_state_for_domain > OG_STATE_ACTIVE): ?>
