e3c2afdecba96f6a63823c97a9426e07f48a2d88
null <null@users.noreply.github.com>
2020-10-30 14:28:17 +0100

    messages: limit hoch: fixes card

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -9,7 +9,7 @@ drush vset maintenance_mode 1
 drush cc all
 
 #revert single features
-drush fr  nm_section_courses_features nm_section_content_features nm_general_features --yes
+drush fr  nm_section_courses_features nm_section_content_features nm_general_features notification_features --yes
 #drush fr annvid_features nm_section_content_features nm_section_courses_features nm_general_features section_projects_features nm_h5p_features section_courses_clone_features --yes
 
 drush php-eval 'node_access_rebuild();'
