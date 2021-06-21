4c8f221a5796799de94163e4cb1479410d404a88
null <null@users.noreply.github.com>
2021-06-21 12:31:09 +0200

    module update h5p

--- web/sites/all/modules/h5p/library/js/h5p.js
+++ web/sites/all/modules/h5p/library/js/h5p.js
@@ -378,8 +378,11 @@ H5P.init = function (target) {
   // Insert H5Ps that should be in iframes.
   H5P.jQuery('iframe.h5p-iframe:not(.h5p-initialized)', target).each(function () {
     var contentId = H5P.jQuery(this).addClass('h5p-initialized').data('content-id');
+    const contentData = H5PIntegration.contents['cid-' + contentId];
+    const language = contentData && contentData.metadata && contentData.metadata.defaultLanguage
+      ? contentData.metadata.defaultLanguage : 'en';
     this.contentDocument.open();
-    this.contentDocument.write('<!doctype html><html class="h5p-iframe"><head>' + H5P.getHeadTags(contentId) + '</head><body><div class="h5p-content" data-content-id="' + contentId + '"/></body></html>');
+    this.contentDocument.write('<!doctype html><html class="h5p-iframe" lang="' + language + '"><head>' + H5P.getHeadTags(contentId) + '</head><body><div class="h5p-content" data-content-id="' + contentId + '"/></body></html>');
     this.contentDocument.close();
   });
 };
@@ -642,7 +645,7 @@ H5P.fullScreen = function ($element, instance, exitCallback, body, forceSemiFull
     // Create real fullscreen.
 
     before('h5p-fullscreen');
-    var first, eventName = (H5P.fullScreenBrowserPrefix === 'ms' ? 'MSFullscreenChange' : (H5P.fullScreenBrowserPrefix === 'moz'?'fullscreenchange' : H5P.fullScreenBrowserPrefix + 'fullscreenchange'));
+    var first, eventName = (H5P.fullScreenBrowserPrefix === 'ms' ? 'MSFullscreenChange' : H5P.fullScreenBrowserPrefix + 'fullscreenchange');
     document.addEventListener(eventName, function () {
       if (first === undefined) {
         // We are entering fullscreen mode
