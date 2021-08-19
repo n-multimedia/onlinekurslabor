0a71506bf70a0c48452d26291c5ef76e8486294f
null <null@users.noreply.github.com>
2021-08-19 14:26:23 +0200

    prepare alpha19-update

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -5,37 +5,17 @@
 drush vset maintenance_mode 1
 
 
-rsync -va --remove-source-files    ../files_private/profile sites/default/files/
-rsync -va --remove-source-files    ../files_private/avatar_* sites/default/files/profile/
-rsync -va --remove-source-files    ../files_private/courses/ sites/default/files/courses_preview
-
 #bug in drush - cc before FR!!
 drush cc all
 
-drush en onlinekurslabor_cache --yes
-
 #revert single features
-drush fr  nm_section_courses_features nm_section_content_features nm_general_features notification_features nm_h5p_features nm_stream_features videosafe_features annvid_features help_features home_features nm_general_features nm_login_vhb_features  section_courses_clone_features videosafe_features  --yes
+#drush fr   --yes
 #drush fr annvid_features nm_section_content_features nm_section_courses_features nm_general_features section_projects_features nm_h5p_features section_courses_clone_features --yes
 
-drush image-flush --all
-
-
-
 #import language
-drush language-import-translations de ../language/alpha18.po --replace --groups=default
+drush language-import-translations de ../language/alpha19.po --replace --groups=default
 
 drush updatedb --yes
-
-drush php-eval 'node_access_rebuild();'
-
-drush en og_forum_D7_fix --yes
-drush en onlinekurslabor_delete_cascade --yes
-
-
-drush vset user_password_reset_timeout 172800
-
-drush php-eval "devel_rebuild_node_comment_statistics();"
 
 drush cc all
 
