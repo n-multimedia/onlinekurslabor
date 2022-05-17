8f01b8f7f1e89abaa55d0e0d6b57310fdca2bf65
null <null@users.noreply.github.com>
2022-05-17 14:35:36 +0200

    Notice: Undefined index: und in include()

--- web/sites/all/themes/bootstrap_onlinekurslabor/templates/user-picture.tpl.php
+++ web/sites/all/themes/bootstrap_onlinekurslabor/templates/user-picture.tpl.php
@@ -4,10 +4,18 @@
  */
 
     $profile = profile2_load_by_user($account, 'master');
+    if(!empty($profile->field_photo))
+    {
+      $image_path = $profile->field_photo[LANGUAGE_NONE][0]['uri']; 
+    }
+    else
+    {
+      $image_path = null;
+    }
          print theme_image_style(
                 array(     
                     'style_name' => 'thumbnail',
-                    'path' =>  $profile->field_photo[LANGUAGE_NONE][0]['uri'] ,
+                    'path' =>   $image_path,
                     'attributes' => array(
                     'class' => 'avatar'
                     ),
