1c256d6c799f32970f099b360cb8a6e4b91565a0
null <null@users.noreply.github.com>
2023-02-15 15:07:29 +0100

    fixes: Activitystream: Falsche Zeit

--- scripts/update_master.sh
+++ scripts/update_master.sh
@@ -9,7 +9,7 @@ drush vset maintenance_mode 1
 drush cc all
 
 #revert single features
-drush fr  home_features annvid_features nm_section_content_features nm_section_courses_features nm_general_features --yes
+drush fr  home_features annvid_features nm_section_content_features nm_section_courses_features nm_general_features notification_features --yes
 #drush fr annvid_features nm_section_content_features nm_section_courses_features nm_general_features section_projects_features nm_h5p_features section_courses_clone_features nm_administration nm_uuid_features --yes
 
 #import language
